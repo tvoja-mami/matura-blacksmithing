39dcd257ef4b4ca274605026e3928b7e03a1e087
Jan <janjager843@gmail.com>
2025-11-01 14:42:12 +0100

    v2025.1.11/3

---
+++ Assets/Scripts/Player/PlayerInventory.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+using System.Collections.Generic;
+public class NewEmptyCSharpScript
+{
+    public ItemData testItem;
+    public List<ItemData> items = new List<ItemData>();
+}
+    
