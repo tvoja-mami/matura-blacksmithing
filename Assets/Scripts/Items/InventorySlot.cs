24641a2816f7d5ddee4a1b39b536e14b816c60e1
Jan <janjager843@gmail.com>
2025-11-06 16:27:11 +0100

    v2025.6.11/inventory1
    inventory item script

--- Assets/Scripts/Items/InventorySlot.cs
+++ Assets/Scripts/Items/InventorySlot.cs
@@ -7,11 +7,14 @@ public class InventoryItem : MonoBehaviour
     public Image iconItem;
     public TextMeshProUGUI quantityText;
     public string nameItem;
+    public string descriptionItem;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     
     void Start()
     {
-        
+        icon.sprite = item.icon;
+        iconItem.enabled = true;
+        quantityText.text = quantity.ToString();
     }
 
     // Update is called once per frame
--- Assets/Scripts/Player/PlayerInventory.cs
+++ Assets/Scripts/Player/PlayerInventory.cs
@@ -1,9 +1,41 @@
 using UnityEngine;
 using System.Collections.Generic;
+using System; // Required for Action
 
 public class PlayerInventory : MonoBehaviour
 {
-    public ItemData testItem;
-	public Dictionary<ItemData, int> items = new Dictionary<ItemData, int>();
+    // Use a Dictionary to store items and their quantities
+    public Dictionary<ItemData, int> items = new Dictionary<ItemData, int>();
+
+    // This is a C# Event. Other scripts can "subscribe" to it.
+    // When the inventory changes, we'll invoke this event.
+    public static event Action OnInventoryChanged;
+
+    public void AddItem(ItemData item, int quantity)
+    {
+        if (items.ContainsKey(item))
+        {
+            items[item] += quantity;
+        }
+        else
+        {
+            items.Add(item, quantity);
+        }
+        // Announce that the inventory has changed!
+        OnInventoryChanged?.Invoke();
+    }
+
+    public void RemoveItem(ItemData item, int quantity)
+    {
+        if (items.ContainsKey(item))
+        {
+            items[item] -= quantity;
+            if (items[item] <= 0)
+            {
+                items.Remove(item);
+            }
+            // Announce that the inventory has changed!
+            OnInventoryChanged?.Invoke();
+        }
+    }
 }
-    
